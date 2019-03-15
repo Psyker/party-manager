c93afd326b90b1742b0e9d6d3cb4e274c3af3974
Psyker <bourgoin.theo1@gmail.com>
2019-03-15 16:42:13 +0100

    Add modules to build app and view engine

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import path from "path";
 import winston from "winston";
 const app = express();
 const port = 8080;
 
+app.set("views", path.join(__dirname, "views"));
+app.set( "view engine", "ejs" );
+
 app.get("/", (req: express.Request, res: express.Response) => {
-    res.send("Hello World");
+    res.render("index");
 });
 
 app.listen(port, () => {
